064e6e60e48b052f0cb5ee283570c96f7835ab44
LeoRickli <106999054+leorickli@users.noreply.github.com>
2023-07-23 16:45:40 -0300

    Delete arquivos_sql directory

--- arquivos_sql/criar_tabela_specialOfferProduct.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS Sales.SpecialOfferProduct
-(
-SpecialOfferID INT(2) NOT NULL,
-ProductID INT(3) NOT NULL,
-rowguid VARCHAR(36),
-ModifiedDate DATETIME,
-PRIMARY KEY (SpecialOfferID, ProductID),
-FOREIGN KEY (ProductID) REFERENCES Production.Product(ProductID)
-)
