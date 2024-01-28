36ab70bd675812540832473a176c3912ac8bf1dd
LeoRickli <106999054+leorickli@users.noreply.github.com>
2024-01-28 14:31:49 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ import mysql.connector
 s3_client = boto3.client('s3')
 
 def lambda_handler(event):
-    # Credentials MySQL RDS
+    # MySQL RDS credentials
     host = os.environ.get('host')
     user = os.environ.get('user')
     password = os.environ.get('password')
